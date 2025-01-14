e33d8d0bf702a40315a76252f527a801755516eb
dkim2448 <156384877+dkim2448@users.noreply.github.com>
2025-01-13 18:41:11 -0700

    Update restrictpossibleusernames.js

--- restrictpossibleusernames.js
+++ restrictpossibleusernames.js
@@ -1,3 +1,7 @@
+// const regex = /e+/g; Match e or more than one e (match as many in a row as you can, but at least one)
+// const regex = /\.$/g; Match the end of our statement
+// const regex = /ea*/g; a is optional, but you can match as many if it's found (Theaaaaa -> eaaaaa)
+
 let username = "JackOfAllTrades";
 let userCheck = /(^[a-z])([a-z]+\d*|[\d][\d])$/i; // Change this line
 let result = userCheck.test(username);
