548c39b940b9fc272fedb5b20feb5633d9241750
dkim2448 <156384877+dkim2448@users.noreply.github.com>
2025-01-21 11:59:09 -0700

    Update mutations.js

--- mutations.js
+++ mutations.js
@@ -1,13 +1,13 @@
 function mutation(arr) {
-    let word1 = arr[0].toLowerCase();
-    let word2 = arr[1].toLowerCase();
+    const word1 = arr[0].toLowerCase();
+    const word2 = arr[1].toLowerCase();
   
     for(let i = 0; i < word2.length; i++){
-        let letter = word2[i];
-        if(word1.indexOf(letter) == -1){
+        if(word1.indexOf(word2[i]) === -1){
             return false;
         }
     }
+    
     return true;
 }
   
